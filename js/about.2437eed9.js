646c487599707192ae06671cba2f8c3cfc20c100
slycatgirl <slycatgirl.band@gmail.com>
2022-05-13 23:54:25 +0900

    Updates

---
+++ js/about.2437eed9.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkhome"]=self["webpackChunkhome"]||[]).push([[443],{4335:function(n,e,u){u.r(e),u.d(e,{default:function(){return f}});var t=u(3396);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function c(n,e){return(0,t.wg)(),(0,t.iD)("div",a,s)}var o=u(89);const i={},h=(0,o.Z)(i,[["render",c]]);var f=h}}]);
+//# sourceMappingURL=about.2437eed9.js.map
